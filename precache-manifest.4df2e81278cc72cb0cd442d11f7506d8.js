34edc15e22929fcd450a5e74bf92ac3df938ce00
zakaos <zakaos@gmail.com>
2020-04-01 16:58:37 -0700

    Updates

---
+++ precache-manifest.4df2e81278cc72cb0cd442d11f7506d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862488396a2e7e5e5b2085bf37618f82",
+    "url": "/todonelive/index.html"
+  },
+  {
+    "revision": "7bb214aa9e2da64d456a",
+    "url": "/todonelive/static/css/main.8534eb2e.chunk.css"
+  },
+  {
+    "revision": "bc641c0e8086a0a15e74",
+    "url": "/todonelive/static/js/2.3845f502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todonelive/static/js/2.3845f502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb214aa9e2da64d456a",
+    "url": "/todonelive/static/js/main.03de1266.chunk.js"
+  },
+  {
+    "revision": "4c1197fed9f488c069b4",
+    "url": "/todonelive/static/js/runtime-main.173d2256.js"
+  },
+  {
+    "revision": "f1ec670e8c9142a228c4de677cfedf78",
+    "url": "/todonelive/static/media/bgtex.f1ec670e.jpg"
+  }
+]);
